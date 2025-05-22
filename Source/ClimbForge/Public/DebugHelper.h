3de64dd243b8c0819ba47a0ea3f69d77c4af20f7
Samarth Shroff <samarth.shroff@gmail.com>
2025-05-21 23:20:03 -0400

    Helper for debugging.

---
+++ Source/ClimbForge/Public/DebugHelper.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+namespace Debug
+{
+	static void Print(const FString& Message, const FColor Color = FColor::MakeRandomColor(), const int32 InKey = -1)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(InKey, 6.0f, Color, Message);
+		}
+
+		UE_LOG(LogTemp, Display, TEXT("%s"), *Message);		
+	}
+}
